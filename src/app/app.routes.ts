ce6190ca1d2452c322bd384af5e98bb0bd41466c
MrArmanDev <rootx.quantum@gmail.com>
2025-05-25 15:18:07 +0530

    profile section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './Dashboard-Module/home-page/home-page.component';
 import { LogoPageComponent } from './logo/logo-page/logo-page.component';
 import { AddItemComponent } from './Dashboard-Module/add-item/add-item.component';
+import { UserProfilePageComponent } from './Profile/user-profile-page/user-profile-page.component';
 
 
 export const routes: Routes = [
    {path: "", component: LogoPageComponent},
    {path: "home", component: HomePageComponent },
-   {path: "add-item", component: AddItemComponent}
+   {path: "add-item", component: AddItemComponent},
+   {path: "profile", component: UserProfilePageComponent}
 ];
