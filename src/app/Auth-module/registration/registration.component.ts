b039eacfcdeaebb954f1d74614a168418fa0c06d
ayushiprusty <ayushi4910@gmail.com>
2025-05-29 15:10:01 +0530

    Added Registration page

--- src/app/Auth-module/registration/registration.component.ts
+++ src/app/Auth-module/registration/registration.component.ts
@@ -1,11 +1,67 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  fullName: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
+  showPassword: boolean = false;
+
+  isOtpScreen = false;
+  otp: string[] = ['', '', '', ''];
+
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+onRegister(): void {
+  if (!this.fullName || !this.email || !this.password || !this.confirmPassword) {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  if (this.password !== this.confirmPassword) {
+    alert('Passwords do not match');
+    return;
+  }
+
+  
+  console.log('Registration submitted');
+  this.isOtpScreen = true;
 }
+
+  onVerify(): void {
+    const enteredOtp = this.otp.join('');
+    if (enteredOtp.length === 4) {
+      // Call verify API here
+      console.log('OTP Verified:', enteredOtp);
+    }
+  }
+
+  onEditEmail(): void {
+    this.isOtpScreen = false;
+  }
+
+  resendOtp(): void {
+   
+    console.log('Resend OTP to', this.email);
+  }
+
+  moveToNext(index: number, event: any) {
+    const input = event.target;
+    if (input.value && index < 3) {
+      const nextInput = document.getElementById(`otp${index + 1}`);
+      if (nextInput) nextInput.focus();
+    }
+  }
+}
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LogoPageComponent } from './logo/logo-page/logo-page.component';
 import { AddItemComponent } from './Dashboard-Module/add-item/add-item.component';
 import { UserProfilePageComponent } from './Profile/user-profile-page/user-profile-page.component';
 import { LoginPageComponent } from './Auth-module/login-page/login-page.component';
+import { RegistrationComponent } from './Auth-module/registration/registration.component';
 
 export const routes: Routes = [
    {path: "", component: LogoPageComponent},
    {path: "home", component: HomePageComponent },
    {path: "add-item", component: AddItemComponent},
    {path: "profile", component: UserProfilePageComponent},
-   { path: "login", component: LoginPageComponent }
+   { path: "login", component: LoginPageComponent },
+    { path: "registration", component: RegistrationComponent },
 ];
